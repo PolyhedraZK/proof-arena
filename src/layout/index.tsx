d0cfe456e04f2e24effd48abeb3a71678b7577fa
liujiachi <jiachi.liu@polyhedra.network>
2024-07-25 16:35:26 +0800

    fix: login replace problems

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -37,10 +37,10 @@ function ProofLayout() {
     //   return navigate('/login', { replace: true });
     // } else {
     //   update('user', { id: userIdCookie });
-      if (localStorage.getItem('token')!== null) {
-        return navigate('/problems', { replace: true });
-      }else{
+      if (localStorage.getItem('token')=== null) {
         return navigate('/login', { replace: true });
+      }else if(location.pathname === '/'){
+        return navigate('/problems', { replace: true })
       }
     // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
