a500cfc127d09b919d91e8a73ab2704c7e325c78
GassaPolyhedra <gassa@polyhedra.network>
2024-07-28 09:54:55 +0800

    fix build error for feat and dev

--- build.sh
+++ build.sh
@@ -3,16 +3,17 @@
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Run the "production" script in `package.json` on the "production" branch
   # "production" should be replaced with the name of your Production branch
-
+  echo "deploy production at" + ${CF_PAGES_URL}
   pnpm run build:production
 
 elif [ "$CF_PAGES_BRANCH" == "preview" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
-
+  echo "deploy production at" + ${CF_PAGES_URL}
   pnpm run build:production
 
 else
   # Else run the dev script
+  echo "VITE_APP_BASE_URL=" + ${CF_PAGES_URL} > ./.env.testing
   pnpm run build:testing
 fi
