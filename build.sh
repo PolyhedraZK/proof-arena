272187648ccecd4631db4920beea55f375ff9354
GassaPolyhedra <gassa@polyhedra.network>
2024-07-28 09:57:17 +0800

    add dev env

--- build.sh
+++ build.sh
@@ -9,9 +9,12 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
 elif [ "$CF_PAGES_BRANCH" == "preview" ]; then
   # Run the "staging" script in `package.json` on the "staging" branch
   # "staging" should be replaced with the name of your specific branch
-  echo "deploy production at" + ${CF_PAGES_URL}
+  echo "deploy preview at" + ${CF_PAGES_URL}
   pnpm run build:production
 
+elif [ "$CF_PAGES_BRANCH" == "dev" ]; then
+  echo "deploy dev at" + ${CF_PAGES_URL}
+  pnpm run build:dev
 else
   # Else run the dev script
   echo "VITE_APP_BASE_URL=" + ${CF_PAGES_URL} > ./.env.testing
