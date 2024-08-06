01f7d60d60641c0cdd54e8dfebd0d8fc466933da
coda <coda@polyhedra.network>
2024-08-06 11:06:13 +0800

    feat: support dev server

--- packages/md-plugin/src/index.ts
+++ packages/md-plugin/src/index.ts
@@ -1,45 +1,11 @@
-import { join } from 'node:path';
+import type { Plugin } from 'vite';
 
-import type { Plugin, ResolvedConfig } from 'vite';
+import { buildPlugin } from './build';
+// dev server plugin
+import { servePlugin } from './serve';
 
-import { parseProblem } from './problemParser';
-import { readDirectories, writeFile } from './util';
-
-const vitePluginDocsParse = (): Plugin => {
-  let config: ResolvedConfig;
-  const problemData: Array<any> = [];
-  return {
-    name: 'vite-plugin-docs-parser',
-    // enforce: 'pre',
-    configResolved(resolvedConfig: ResolvedConfig) {
-      config = resolvedConfig;
-    },
-    // transform(code, id) {
-    //   source_map = tf(code, id);
-    //   return source_map;
-    // },
-    async buildEnd() {
-      // 读取文件
-      const { root } = config;
-      const docsPath = join(root, '../../', 'docs');
-      console.log(`docsPath = ${docsPath}`);
-      const files = await readDirectories(docsPath);
-      for (const file of files) {
-        const problemInfo = await parseProblem(docsPath, file);
-        problemData.push({
-          ...problemInfo.metadata,
-          details: problemInfo.details,
-          submission_data_path: `/docs/${file}/submissions.json`,
-        });
-      }
-    },
-    async writeBundle() {
-      const { root, build } = config;
-      const destDir = join(root, build.outDir);
-      console.log(`destFile = ${destDir}`);
-      await writeFile(destDir, 'problemData.json', JSON.stringify(problemData));
-    },
-  };
+const vitePluginDocsParse = (): Plugin[] => {
+  return [servePlugin(), buildPlugin()];
 };
 
 export default vitePluginDocsParse;
