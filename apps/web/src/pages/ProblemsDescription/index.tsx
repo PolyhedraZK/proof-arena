1266d8c22ac1376a4c6137a71d4c5dc653b1f018
liujiachi <jiachi.liu@polyhedra.network>
2024-08-07 14:50:12 +0800

    feat: add md highlighter

--- apps/web/src/pages/ProblemsDescription/index.tsx
+++ apps/web/src/pages/ProblemsDescription/index.tsx
@@ -1,8 +1,32 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { github } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const ProblemsDescription = ({ mdFile }: { mdFile: string }) => {
-  return <Markdown remarkPlugins={[remarkGfm]}>{mdFile}</Markdown>;
+  return <Markdown
+    children={mdFile}
+    remarkPlugins={[remarkGfm]}
+    components={{
+      code(props) {
+        const { children, className, node, ...rest } = props
+        const match = /language-(\w+)/.exec(className || '')
+        return match ? (
+          <SyntaxHighlighter
+            {...rest}
+            PreTag="div"
+            children={String(children).replace(/\n$/, '')}
+            language={match[1]}
+            style={github}
+          />
+        ) : (
+          <code {...rest} className={className}>
+            {children}
+          </code>
+        )
+      }
+    }}
+  />
 };
 
 export default ProblemsDescription;
