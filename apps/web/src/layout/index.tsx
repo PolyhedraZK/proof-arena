cfa36ae85d42f9f53031c9060b40abf3b8175870
Tiancheng Xie <tc@polyhedra.network>
2024-08-17 22:25:25 -0700

    home

--- apps/web/src/layout/index.tsx
+++ apps/web/src/layout/index.tsx
@@ -1,22 +1,12 @@
 import { Layout } from 'antd';
-import { useEffect } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router';
+import { Outlet } from 'react-router';
 
 import Header from './Header';
 import useStyles from './layout.style';
 import Footer from './Footer';
 
 function ProofLayout() {
-  const navigate = useNavigate();
-  const location = useLocation();
   const { styles } = useStyles();
-
-  // 监听路由变化的时候
-  useEffect(() => {
-    if (location.pathname === '/') {
-      return navigate('/problems', { replace: true });
-    }
-  }, [location]);
 
   return (
     <div className={styles.layout}>
