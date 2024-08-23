5ddeec2a6c0186552658fb151c589f1300ac55dc
Tiancheng Xie <tc@polyhedra.network>
2024-08-22 17:54:42 -0700

    gnark submission [script:problems/keccak256_hash/expectedProver/run.sh]

--- problems/keccak256_hash/expectedProver/run.sh
+++ problems/keccak256_hash/expectedProver/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go build -C ../SPJ
-go build
+go build -C problems/keccak256_hash/expectedProver
+go build -C problems/keccak256_hash/SPJ
 
-../SPJ/SPJ -cpu 64 -largestN 1024 -memory 1024 -prover "./expectedProver -mode prove" -time 300 -verifier "./expectedProver -mode verify" -json "../../../spj_output/keccak256_hash/3-GNARK_KECCAK-256.json"
+problems/keccak256_hash/SPJ/SPJ -cpu 64 -largestN 1024 -memory 1024 -prover "problems/keccak256_hash/expectedProver/expectedProver -mode prove" -time 300 -verifier "problems/keccak256_hash/expectedProver/expectedProver -mode verify" -json "spj_output/keccak256_hash/3-GNARK_KECCAK-256.json"
