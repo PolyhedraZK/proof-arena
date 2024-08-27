23fc18646fba11bc03b74c64d45a6269d6d04f9d
zhenfei <zhenfei.zhang@hotmail.com>
2024-08-26 22:04:23 -0400

    remove workspace [script:problems/AXorB/halo2/run-halo2-xor-circuit.sh]

--- problems/AXorB/halo2/run-halo2-xor-circuit.sh
+++ problems/AXorB/halo2/run-halo2-xor-circuit.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# execute me at the [proof-area] directory
+
 set -x  # Enable command echoing for debugging
 
 # Remove old pipes and log file
 rm -f /tmp/spj_to_prover /tmp/prover_to_spj /tmp/spj_to_verifier /tmp/verifier_to_spj 
 
 # build rust binaries
-cargo build --release -p halo2_xor_circuit
+cd problems/AXorBpw/halo2/ && cargo build --release -p halo2_xor_circuit && cd -
 
 # build go SPJ
 go mod -C problems/AXorB/expectedProver tidy
@@ -14,14 +16,12 @@ go mod -C problems/AXorB/SPJ tidy
 go build -C problems/AXorB/expectedProver
 go build -C problems/AXorB/SPJ
 
-
-
 # Run the SPJ
-problems/AXorB/SPJ/SPJ -cpu 16 -largestN 136 \
+problems/AXorB/SPJ/SPJ -cpu 16 -largestN 10919 \
   -memory 32768 -time 1200 \
   -json spj_output/AXorB/Halo2-Xor-256.json \
-  -prover "target/release/prover" \
-  -verifier "target/release/verifier"
+  -prover "problems/AXorBpw/halo2/target/release/prover" \
+  -verifier "problems/AXorBpw/halo2/target/release/verifier"
 
 # Capture the exit status
 exit_status=$?
